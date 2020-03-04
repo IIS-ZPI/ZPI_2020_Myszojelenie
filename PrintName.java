22a2a5e6913aad00939642cbe65bf99d3361cb2e
AdamWojtczak <215886@edu.p.lodz.pl>
2020-03-04 13:40:11 +0100

    zadanie4

--- PrintName.java
+++ PrintName.java
@@ -1,6 +1,11 @@
-public class PrintName {
+public class PrintName implements IArithmeticsDiff {
+
     public static void main(String[] args) {
         System.out.println("Myszojelenie Operations snapeeek");
     }
 
+    @Override
+    public double Difference(double A, double B) {
+        return A-B;
+    }
 }
