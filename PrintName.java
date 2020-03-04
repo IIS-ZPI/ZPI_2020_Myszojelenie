37c1f990391b8a03fed0e94b9b817748b9959b19
AdamWojtczak <215886@edu.p.lodz.pl>
2020-03-04 13:31:13 +0100

    zadanie2 nowy

--- PrintName.java
+++ PrintName.java
@@ -1,6 +1,7 @@
 public class PrintName {
     public static void main(String[] args) {
         System.out.println("Myszojelenie Operations snapeeek");
+        System.out.println("Developer AdamWojtczak");
     }
 
 }
