ff9c07de7a14c7eb6cfb0a4a84c37d106b23efa6
AdamWojtczak <215886@edu.p.lodz.pl>
2020-03-04 13:42:14 +0100

    zadanie4

--- PrintName.java
+++ PrintName.java
@@ -1,7 +1,11 @@
-public class PrintName implements IArithmeticsDiff {
-
+public class PrintName implements IArithmeticsDiff{
     public static void main(String[] args) {
+
         System.out.println("Myszojelenie Operations snapeeek");
+        System.out.println("Developer AdamWojtczak");
+        System.out.println("Tester AndrzejBilant");
+        System.out.println("Operations Snapeeek");
+        System.out.println("Developer BartoszKowalczyk98");
     }
 
     @Override
