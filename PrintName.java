30a9b3b0cef84d948b1715d5230a364e745dd988
AdamWojtczak <215886@edu.p.lodz.pl>
2020-03-04 13:22:29 +0100

    zadanie2opoprawa

--- PrintName.java
+++ PrintName.java
@@ -1,7 +1,7 @@
 public class PrintName {
     public static void main(String[] args) {
         System.out.println("Myszojelenie Operations snapeeek");
-        System.out.println("Myszojelenie Developer  AdamWojtczak");
+        System.out.println("Myszojelenie Developer AdamWojtczak");
     }
 
 }
