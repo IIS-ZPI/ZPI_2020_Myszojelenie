53fa9759f320e87552952a55d84ca152062d814a
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-03-25 12:56:33 +0100

    AB zad5.1

--- IArithmeticsAdd.java
+++ IArithmeticsAdd.java
@@ -1,3 +1,8 @@
 public interface IArithmeticsAdd {
     double Addition(double A, double B);
+    /*Myszo jeleń!
+    Zagrożony wyginięciem
+    Myszo jeleń!
+    Najmniejszy przeżuwacz świata
+    */
 }
