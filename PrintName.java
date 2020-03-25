fec586c8b0bcfeda4e1fc447504cec8eb3d473aa
snapeeek <maciekwozniak97@gmail.com>
2020-03-25 13:03:53 +0100

    Update PrintName.java

--- PrintName.java
+++ PrintName.java
@@ -8,6 +8,11 @@ public class PrintName implements IArithmeticsDiv, IArithmeticsDiff, IArithmetic
         System.out.println("Tester AndrzejBilant");
         System.out.println("Operations Snapeeek");
         System.out.println("Developer BartoszKowalczyk98");
+
+
+        //now am git
+        //haha
+        //lame joke sry
     }
 
 
