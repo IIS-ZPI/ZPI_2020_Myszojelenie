0a710586cece7fafde0a1363770ba383ea284ed1
Kowalczyk.Bartosz <21577@edu.p.lodz.pl>
2020-05-12 16:55:24 +0200

    refaktoryzacja

--- Myszojelen/urls.py
+++ Myszojelen/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='base.html'), name='baza'),
     path('', views.index, name='index'),
     path('adding/', views.adding_form, name='adding')
 ]
