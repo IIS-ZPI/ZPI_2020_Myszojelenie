7ee096562fb64a46f913a7ae0eb442cd7d9ae668
Kowalczyk.Bartosz <21577@edu.p.lodz.pl>
2020-05-12 18:27:22 +0200

    Multichoice do stanów
    Co-Authored-By: adamwojtczak <adamwojtczak@users.noreply.github.com>

--- Myszojelen/urls.py
+++ Myszojelen/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'^select2/', include('django_select2.urls')),
     path('adding/', views.adding_form, name='adding')
 ]
--- Myszojelen/views.py
+++ Myszojelen/views.py
@@ -13,28 +13,31 @@ def index(request):
     }
     if request.method == 'POST':
         data = request.POST.copy()
-        selling_price = float(data.get('selling_price'))
-        state = State_Category_Tax.objects.filter(id_cat=int(data.get('id_cat')),
-                                                  id_state=int(data.get('id_state')))
-        product_price = float(data.get('product_price'))
-        if state.exists():
-            tax = state[0].tax_val
-            state_name = state[0].id_state.state_name
+        print(data.get('id_state'))
+        xdicktionary = dict(data.lists())
+        for stateid in xdicktionary.get('id_state'):
+            selling_price = float(data.get('selling_price'))
+            state = State_Category_Tax.objects.filter(id_cat=int(data.get('id_cat')),
+                                                      id_state=int(stateid))
+            product_price = float(data.get('product_price'))
+            if state.exists():
+                tax = state[0].tax_val
+                state_name = state[0].id_state.state_name
 
-        else:
-            stan = State.objects.get(id=int(data.get('id_state')))
-            state_name = stan.state_name
-            tax = stan.state_base_tax
-        category = Category.objects.get(id=int(data.get('id_cat')))
-        zysk = round((selling_price - product_price * (1 + tax)) / (1 + tax), 4)
-        list_of_calculation_holders.append(
-            CalculationsHolder(state=state_name,
-                               cat=category.category_name,
-                               pronam=data.get('product_name'),
-                               prounipr=product_price,
-                               proselpri=selling_price,
-                               calpri=zysk))
-        list_of_calculation_holders.sort()
+            else:
+                stan = State.objects.get(id=int(stateid))
+                state_name = stan.state_name
+                tax = stan.state_base_tax
+            category = Category.objects.get(id=int(data.get('id_cat')))
+            zysk = round((selling_price - product_price * (1 + tax)) / (1 + tax), 4)
+            list_of_calculation_holders.append(
+                CalculationsHolder(state=state_name,
+                                   cat=category.category_name,
+                                   pronam=data.get('product_name'),
+                                   prounipr=product_price,
+                                   proselpri=selling_price,
+                                   calpri=zysk))
+            list_of_calculation_holders.sort()
 
     return render(request, "Myszojelen/index.html", context)
 
