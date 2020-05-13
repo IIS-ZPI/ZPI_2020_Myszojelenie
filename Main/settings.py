5139566fad000cce560dea6ef8d779ad99123ec9
Kowalczyk.Bartosz <21577@edu.p.lodz.pl>
2020-05-13 15:30:08 +0200

    wykomentowanie heroku i podpięcie django_select2

--- Main/settings.py
+++ Main/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'Myszojelen.apps.MyszojelenConfig',
+    'django_select2',
     'Myszojelen'
 ]
 
@@ -124,4 +124,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
